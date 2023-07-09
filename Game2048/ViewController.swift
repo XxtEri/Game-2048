6e19fcb6a9bf675420fb1d79e3ff9fd9825c3af1
Gerasimchuk Elena <zziikk2003@gmail.com>
2023-07-09 22:59:35 +0700

    Initial Commit

---
+++ Game2048/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Game2048
+//
+//  Created by Елена on 09.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
